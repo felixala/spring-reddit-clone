a8e1f28bb2aee6fa273b9b17af2772dfe6eb4af7
Felix Laura <felixgato8@gmail.com>
2021-04-30 23:42:15 -0400

    Creating token for registration

--- src/main/java/com/felixlaura/springredditclone/service/AuthService.java
+++ src/main/java/com/felixlaura/springredditclone/service/AuthService.java
@@ -2,13 +2,16 @@ package com.felixlaura.springredditclone.service;
 
 import com.felixlaura.springredditclone.dto.RegisterRequest;
 import com.felixlaura.springredditclone.model.User;
+import com.felixlaura.springredditclone.model.VerificationToken;
 import com.felixlaura.springredditclone.repository.UserRepository;
+import com.felixlaura.springredditclone.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +19,7 @@ public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
 
     /**
      * signup method responsible to create User object through registration process and storing in the database
@@ -30,5 +34,19 @@ public class AuthService {
         user.setEnabled(false);
         userRepository.save(user);
 
+        String token = generateVerificationToken(user);
+    }
+
+    /**
+     * create a new token
+     */
+    public String generateVerificationToken(User user){
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 }
